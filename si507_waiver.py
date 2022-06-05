ec1f28ad4f63430f72148ec901fd21d589eaa265
prithvidasgupta <106804681+prithvidasgupta@users.noreply.github.com>
2022-06-05 21:24:13 +0530

    final code

--- si507_waiver.py
+++ si507_waiver.py
@@ -1,10 +1,13 @@
+"""
+"""
 def read_file():
     file = open('./BaconData/BaconCastFull.txt')
     movies = file.readlines()
     file.close()
     return movies
 
-
+"""
+"""
 def generateNetwork(movies):
     actorNetwork = {}
     allActors = set()
@@ -26,8 +29,6 @@ def generateNetwork(movies):
 """
 
 """
-
-
 def generateMinimumPath(actr1, actr2, listAllActors, actorNetwork):
     queue = []
     queue.append(actr1)
@@ -56,7 +57,8 @@ def generateMinimumPath(actr1, actr2, listAllActors, actorNetwork):
                 return minmumPath
     return minmumPath
 
-
+"""
+"""
 def reconstructPath(actor1, actor2, minmumPath, listAllActors):
     path = []
     temp = listAllActors.index(actor2)
@@ -69,42 +71,70 @@ def reconstructPath(actor1, actor2, minmumPath, listAllActors):
         return path
     return []
 
-
-def generateBaconNumbers(listAllActors, actorNetwork):
+"""
+"""
+def generateBaconNumbers(actorNetwork):
     bacon = 'Bacon, Kevin'
-    queue = []
-    queue.append(bacon)
-    levelCount = 1
-    levelSize = len(queue)
-    baconNumbers = [None for i in range(len(listAllActors))]
-    baconNumbers[listAllActors.index(bacon)] = 1
+    currentSet = set()
+    currentSet.add(bacon)
+    nextSet = set()
+    actorCounts = list()
+    processedSet = set()
+    run = True
+    while run:
+        actorCount = 0
+        for actor in currentSet:
+            if actor in processedSet:
+                continue 
+            nextSet.update(actorNetwork[actor])
+            actorCount+=1
+            processedSet.add(actor)
+        if len(nextSet)==0:
+            run = False
+        else:
+            actorCounts.append(actorCount)
+            currentSet=nextSet
+            nextSet = set()
+    return actorCounts
 
-    print(len(actorNetwork[bacon]))
-
-    while queue:
-        if (levelSize == 0):
-            levelCount += 1
-            levelSize = len(queue)
-        actor = queue[0]
-        queue.pop(0)
-        print(levelSize)
-        print(levelCount)
-        for co_actor in actorNetwork[actor]:
-            if baconNumbers[listAllActors.index(co_actor)]:
-                continue
-            baconNumbers[listAllActors.index(co_actor)] = levelCount
-            queue.append(co_actor)
-        levelSize -= 1
-    filter(None, baconNumbers)
-    return baconNumbers
-
-
+"""
+"""
 def search(actor1, actor2, listAllActors, actorNetwork):
     minmumPath = generateMinimumPath(
         actor1, actor2, listAllActors, actorNetwork)
     return reconstructPath(actor1, actor2, minmumPath, listAllActors)
+"""
+[1, 2249, 218085, 561132, 111180, 7906, 903, 100, 14]
+Haaland, Daniel
+Maciel, Henri
+Adams, Joanna
+Neder, Hermelino
+Shafer, Joseph Otero
+Bozoyan, Mariam
+"""
+def generateBaconMedianMean(actorNetwork):
+    numbers = generateBaconNumbers(actorNetwork)
+    totalActorsInBaconTree = 0
+    mean = 0;
+    median = 0;
+    temp = 0;
+    mid =0;
+    totalActorsInBaconTree = sum(numbers)
+    if(totalActorsInBaconTree%2==0):
+        mid=int((totalActorsInBaconTree/2)-1)
+    else:
+        mid = int(totalActorsInBaconTree/2)
+    for number in numbers:
+        temp+=number
+        if median==0 and mid<temp:
+            median = numbers.index(number)
+        mean+=number*numbers.index(number)
+    mean = mean/totalActorsInBaconTree
+    return median,mean
 
 
+"""
+"""
 def getActorInput(actorNumber, actorNetwork):
     while True:
         actor = input('Enter name of actor '+str(actorNumber) +
@@ -120,10 +150,11 @@ def getActorInput(actorNumber, actorNetwork):
             print('Actor not found. Please enter again')
             continue
 
-
-def getContinueInput():
+"""
+"""
+def getContinueInput(str):
     while True:
-        cont = input('Continue finding degree of separation? (yes/no) ')
+        cont = input(str+' (yes/no): ')
         if cont.lower().strip() == 'exit':
             quit()
         elif cont.lower() == 'yes':
@@ -134,17 +165,18 @@ def getContinueInput():
             print('Invalid input. Please enter "yes" or "no"')
             continue
 
-
+"""
+"""
 def formatOutput(actor):
     actor = actor.split(', ')
     actor.reverse()
     return ' '.join(actor)
 
-
+"""
+"""
 def run():
     movies = read_file()
     actorNetwork, listAllActors = generateNetwork(movies)
-    #median,mean = generateBaconMedianMean(actorNetwork)
     print('Find the degree of separation between two actors!')
     while True:
         actor1 = getActorInput(1, actorNetwork)
@@ -155,18 +187,16 @@ def run():
         else:
             print('Path: '+' -> '.join(map(formatOutput, path)))
             print('Degree of separation: '+str(len(path)-1))
-        cont = getContinueInput()
+        cont = getContinueInput('Continue finding degree of separation?')
         if(cont):
             continue
         else:
-            # print('Median Bacon Number: '+str(median))
-            # print('Mean Bacon Number (3 decimal places): '+str(mean))
+            seeBaconNums = getContinueInput('See the median and mean of Kevin Bacon\'s degree of separation?')
+            if(seeBaconNums):
+                 median,mean = generateBaconMedianMean(actorNetwork)
+                 print('Median: '+str(median))
+                 print('Mean: '+str(round(mean,3)))
             quit()
 
 
-# run()
-
-movies = read_file()
-actorNetwork, listAllActors = generateNetwork(movies)
-nums = generateBaconNumbers(listAllActors, actorNetwork)
-print(len(nums))
+run()
